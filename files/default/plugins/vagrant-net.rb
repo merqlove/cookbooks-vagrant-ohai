dee8bfe9f356b7f1cfa669df5946a5b7ecbb9987
Alexander Merkulov <sasha@merqlove.ru>
2014-07-27 19:19:30 +0400

    Upgrade to Ohai 7

--- files/default/plugins/vagrant-net.rb
+++ files/default/plugins/vagrant-net.rb
@@ -1,10 +1,15 @@
 
 # borrowed from https://gist.github.com/2050259
 
-provide "ipaddress"
-require_plugin "#{os}::network"
-network["interfaces"]["eth1"]["addresses"].each do |ip, params|
-  if params['family'] == ('inet')
-    ipaddress ip
+Ohai.plugin(:ipaddress) do
+  provide "ipaddress"
+  depends "#{os}::network"
+  collect_data(:network) do
+    network["interfaces"]["eth1"]["addresses"].each do |ip, params|
+      if params['family'] == ('inet')
+        ipaddress ip
+      end
+    end"
   end
 end
+
